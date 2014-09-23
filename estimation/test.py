a9b2e0099b0ca21256d746e3204b07ee6af6a9a2
qgniu <qinggongniu@gmail.com>
2014-09-23 13:47:48 +0800

    1.Test  'entry_for_packet()': just run estimation.test.testC() 2.Test  openvswitch: run the pox ,then from estimation import test, excute test.testS(). In addition，you need re-edit the directory of test data in testData.py.

--- estimation/test.py
+++ estimation/test.py
@@ -1,41 +1,72 @@
-from pox.openflow.flow_table import FlowTable
-from pox.openflow.flow_table import TableEntry
+
 from pox.lib.addresses import EthAddr
 from pox.lib.addresses import IPAddr
 from pox.openflow.libopenflow_01 import ofp_match
-from pox.openflow.libopenflow_01 import ofp_action_output
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.packet.ethernet import ethernet
-from pox.lib.packet.ipv4 import ipv4
-from pox.lib.packet.udp import udp
+from estimation.timer import Timer
+import time
+import testData
+def entry_for_packet (tables, packet, in_port):
+    packet_match = ofp_match.from_packet(packet, in_port, spec_frags = True)
+    for entry in tables._table:
+        if entry.match.matches_with_wildcards(packet_match,
+                                                consider_other_wildcards=False):
+            break
 def testS():
+    with Timer() as time1:
         msg=of.ofp_flow_mod()
         i=0
-        while i<=10:
-            msg.priority=i
+        while i<10:
+            msg.match.dl_type=0x800
+            msg.match.dl_src=EthAddr("00:03:0f:01:12:4%s"%i)
+            msg.match.nw_proto=6
+            msg.match.nw_dst="1.0.0.0/8"
+            msg.match.nw_src="1.2.3.4"
+            msg.match.tp_src=1
+            msg.match.tp_dst=2
             msg.actions.append(of.ofp_action_output(port=194))
             core.openflow.connections[1].send(msg)
             print str(i)+"***"
             i=i+1
+    print "=>Time: %s s" % time1.secs
+def print_write(strs):
+    result=open(r"..\Data\result",'a')
+    print strs
+    result.write("\n"+strs)
+    result.close()
+def average ():
+    file=open(r"..\Data\result",'r')
+    lines=file.readlines()
+    p=0
+    t=0
+    for line in lines:
+        if line.find('Entry_for_packet')!=-1:
+            tem=line.split('.')
+            t=t+int(filter(str.isdigit,tem[0]))
+        if line.find('number of packets')!=-1:
+            tem=line.split('.')
+            p=p+int(filter(str.isdigit,tem[0]))
+    return p/t
+            
 def testC():
-    def entry_for_packet (tables, packet, in_port):
-        packet_match = ofp_match.from_packet(packet, in_port, spec_frags = True)
-        for entry in tables._table:
-            if entry.match.matches_with_wildcards(packet_match,
-                                                  consider_other_wildcards=True):
-                print"match"
-        print "notmatch"
-    t = FlowTable()
-    t.add_entry(TableEntry(priority=6, cookie=0x1, match=ofp_match(dl_src=EthAddr("00:00:00:00:00:01"),nw_src="1.2.3.4"), actions=[ofp_action_output(port=5)]))
-    t.add_entry(TableEntry(priority=5, cookie=0x2, match=ofp_match(dl_src=EthAddr("00:00:00:00:00:02"), nw_src="1.2.3.0/24"), actions=[ofp_action_output(port=6)]))
-    packet = ethernet(
-        src=EthAddr("00:00:00:00:00:01"),
-        dst=EthAddr("00:00:00:00:00:02"),
-        payload=ipv4(srcip=IPAddr("1.2.3.4"),
-        dstip=IPAddr("1.2.3.5"),
-        payload=udp(srcport=1234, dstport=53, payload="haha")))
-    if packet is None:
-        print "packet is none"
-    else:
-        entry_for_packet(t, packet, 1)
+    ISOTIMEFORMAT='%Y-%m-%d %X'
+    table=testData.GenerateACL()
+    packets=testData.GenerateTrace()
+    pnum=len(packets)
+    tnum=len(table)
+    startTime= time.strftime( ISOTIMEFORMAT, time.localtime( time.time() ) )
+    print_write(startTime)
+    print_write("go!!")
+    with Timer() as time2:
+        for packet in packets:
+            entry_for_packet(table, packet, 1)
+    times=time2.secs
+    print_write( "=>Time of Entry_for_packet: %s s" %  times)
+    print_write( "The size of table is :%s"%tnum)
+    print_write( "The number of packets is:%s "%pnum)
+    print_write( "The speed is:%s packet/s"%(int(pnum/times)))
+    print_write("The averate speed is:%s packet/s"%average())
+    endTime = time.strftime( ISOTIMEFORMAT, time.localtime( time.time() ) )
+    print_write( endTime)
+    print_write( "end.\n**************************************")
--- estimation/testData.py
+++ estimation/testData.py
@@ -3,3 +3,57 @@ Created on Sep 19, 2014
 
 @author: niuni
 '''
+from pox.openflow.flow_table import FlowTable
+from pox.openflow.flow_table import TableEntry
+from pox.lib.addresses import EthAddr
+from pox.lib.addresses import IPAddr
+from pox.openflow.libopenflow_01 import ofp_match
+from pox.openflow.libopenflow_01 import ofp_action_output
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.packet.ethernet import ethernet
+from pox.lib.packet.ipv4 import ipv4
+from pox.lib.packet.udp import udp
+from estimation.timer import Timer
+import socket
+import struct
+import ctypes
+def ip2long (ip):
+    return struct.unpack("!I", socket.inet_aton(ip))[0]
+def long2ip (lint):
+    return socket.inet_ntoa(struct.pack("!I", lint))
+def GenerateACL():
+    acl_rules=open(r'..\Data\acl3_4k_rules.txt')
+    table = FlowTable()
+    i=3874
+    while True:
+        r=acl_rules.readline().split()
+        if not r:break
+        table.add_entry(TableEntry(priority=i, cookie=0x1, match=ofp_match(dl_src=EthAddr("00:00:00:00:00:01"),nw_src=r[0],nw_dst=r[1]), actions=[ofp_action_output(port=5)]))
+            #table.add_entry(TableEntry(priority=i, cookie=0x1, match=ofp_match(dl_src=EthAddr("00:00:00:00:00:01"),nw_src="53.45.14.183/32",nw_dst="18.184.25.126/32"), actions=[ofp_action_output(port=5)]))
+        i=i-1
+    return table
+def GenerateTrace():
+    acl_trace=open(r'..\Data\acl3_4k_rules.txt_trace')
+    trace=acl_trace.readlines()
+    packets=[]
+    for line in trace:
+        tum=line.split()
+        src=long2ip(long(tum[0]))
+        dst=long2ip(long(tum[1]))
+        packet = ethernet(
+                          src=EthAddr("00:00:00:00:00:01"),
+                          payload=ipv4(srcip=IPAddr(src),
+                                       dstip=IPAddr(dst),
+                                       payload=udp(srcport=1234, dstport=53, payload="haha")))
+        packets.append(packet)
+    return packets
+'''
+t=GenerateACL()
+table=t._table
+for i in range(0,len(table)):
+    print "priority is :%s"%(table[i].effective_priority)
+    print table[i].match
+    print "************************************8"
+print len(t._table)
+'''
